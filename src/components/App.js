d246f5e0abe786cf1b66f28faafa5ba7315621ae
Nicholas Gebhart <nicholas.gebhart@gmail.com>
2019-12-21 18:23:59 -0800

    chore: adds routes
    MockHomePage -> testing
    Login -> okta widget

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,33 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { Security, ImplicitCallback } from '@okta/okta-react';
-import { Route } from 'react-router-dom';
-import Home from './Home';
-
-import '../styles/App.css';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Login from './Authorization/Login';
+import MockHomePage from './MockHomePage';
+import PrivateRoute from './Authorization/PrivateRoute';
 
 const config = {
   issuer: `${process.env.REACT_APP_OKTA_URL}/oauth2/default`,
   redirectUri: `${window.location.origin}/implicit/callback`,
   clientId: `${process.env.REACT_APP_CLIENT_ID}`,
+  onAuthRequired({ history }) {
+    history.push('/');
+  },
   pkce: true,
 };
 
 const App = () => (
   <>
+    {/* OKTA Security wrapper */}
     <Security {...config}>
-      <Route exact path="/" component={Home} />
-      <Route path="/implicit/callback" component={ImplicitCallback} />
+      <Switch>
+        <Route
+          path="/login"
+          render={() => <Login baseUrl={`${process.env.REACT_APP_OKTA_URL}`} />}
+        />
+        <Route exact path="/admin/dashboard" component={MockHomePage} />
+        <Route path="/implicit/callback" component={ImplicitCallback} />
+      </Switch>
     </Security>
   </>
 );
